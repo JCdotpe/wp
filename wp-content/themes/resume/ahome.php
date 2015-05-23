403fa3cbc47decf07da96aad391204c18dce0d2e
JCdotpe <mail@jc.pe>
2015-05-23 15:35:25 -0500

    Init

---
+++ wp-content/themes/resume/ahome.php
@@ -0,0 +1,129 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Juan Carlos Sánchez Tapia - Consultor senior de innovación y desarrollo para Startups</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <!-- Fonts from Font Awsome -->
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+         <!-- Magnific popup -->
+        <link rel="stylesheet" href="http://authenticgoods.co/wrapbootstrap/themes/articulate/css/magnific-popup.css">
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="http://authenticgoods.co/wrapbootstrap/themes/articulate/css/main.css">
+
+        <!-- Color styles -->
+        <link rel="stylesheet" href="http://authenticgoods.co/wrapbootstrap/themes/articulate/css/colors/blue.css">
+       <!-- <link rel="stylesheet" href="css/colors/yellow.css">-->
+       <!-- <link rel="stylesheet" href="css/colors/red.css">-->
+       <!--  <link rel="stylesheet" href="css/colors/purple.css">-->
+       <!--  <link rel="stylesheet" href="css/colors/orange.css">-->
+       <!--  <link rel="stylesheet" href="css/colors/green.css">-->
+        
+         <!-- Feature detection -->
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/modernizr-2.6.2.min.js"></script>
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+
+        <style>
+          .vegas-loading{ display: none !important}
+          #page-welcome header p {font-size: 1.5em;}
+        </style>
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/plugins/html5shiv.js"></script>
+          <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/plugins/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+<!--   <div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav" id="main-menu">
+            <li><a href="#page-welcome">Home</a></li>
+            <!--
+            <li><a href="#page-profile">Profile</a></li>
+            <li><a href="#page-skills">Skills</a></li>
+            <li><a href="#page-education">Education</a></li>
+            <li><a href="#page-experience">Experience</a></li>
+            <li><a href="#page-portfolio">Portfolio</a></li>
+            <li><a href="#page-contact">Contacto</a></li>
+          -->
+          <!-- </ul>
+        </div>/.nav-collapse 
+      </div>
+    </div> --> 
+    
+    <!-- welcome begins -->
+    <section id="page-welcome" class="page-welcome">
+      <div>
+      <!--     <ul class="slider-controls">
+              <li><a id="vegas-next" class="next" href="#"></a></li>
+              <li><a id="vegas-prev" class="prev" href="#"></a></li>
+          </ul> -->
+        </div>
+        <div class="container">
+            <div class="row">
+                <header class="centered">
+                    <h1>JUAN CARLOS SANCHEZ TAPIA</h1>
+                    <p>CONSULTOR SENIOR DE INNOVACION Y DESARROLLO TECNOLOGICO</p>
+                </header>
+                <div class="social-icons">
+                        <a href="https://twitter.com/JCdotpe" class="btn btn-round btn-clear btn-twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.facebook.com/JCdotpe" class="btn btn-round btn-clear btn-facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="http://dribbble.com/JCdotpe" class="btn btn-round btn-clear btn-dribbble"><i class="fa fa-dribbble"></i></a>
+                        <a href="http://instagram.com/jcdotpe/" class="btn btn-round btn-clear btn-instagram"><i class="fa fa-instagram"></i></a>
+                        <a href="http://www.linkedin.com/in/jcdotpe" class="btn btn-round btn-clear btn-linkedin"><i class="fa fa-linkedin-square"></i></a>
+                        <a href="http://www.pinterest.com/jcdotpe/" class="btn btn-round btn-clear btn-pinterest"><i class="fa fa-pinterest"></i></a>
+                        <a href="https://plus.google.com/+JuanCarlosSánchezTapia" class="btn btn-round btn-clear btn-google"><i class="fa fa-google-plus"></i></a>
+                    
+                </div>
+             <!--    <a href="#page-contact" class="btn btn-default hire-me">+51 986688867</a> -->
+            </div>
+        </div>
+    </section><!-- welcome ends -->
+
+
+
+
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery-1.10.2.min.js"></script>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.scrollTo.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.nav.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.sticky.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.easypiechart.min.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.vegas.min.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.isotope.min.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.magnific-popup.min.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/jquery.validate.js"></script>
+        <script src="http://authenticgoods.co/wrapbootstrap/themes/articulate/js/waypoints.min.js"></script>
+        <script>
+
+$(document).ready(function(){$(".hire-me").click(function(){return $("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},500),!1}),$("ul.nav-pills li a").click(function(){$("ul.nav-pills li.active").removeClass("active"),$(this).parent("li").addClass("active")}),$(window).load(function(){var e=$(".grid-wrapper");e.isotope({filter:"*",animationOptions:{duration:750,easing:"linear",queue:!1}}),$(".grid-controls li a").click(function(){$(".grid-controls .current").removeClass("current"),$(this).addClass("current");var i=$(this).attr("data-filter");return e.isotope({filter:i,animationOptions:{duration:750,easing:"linear",queue:!1}}),!1})}),$(".grid-wrapper").magnificPopup({delegate:"a",type:"image",gallery:{enabled:!0}}),$(".navbar").sticky({topSpacing:0}),$("#main-menu").onePageNav({currentClass:"active",changeHash:!1,scrollThreshold:.5,scrollSpeed:750,filter:"",easing:"swing"}),$(".chart").waypoint(function(){$(this).easyPieChart({barColor:"#3498db",size:"150",easing:"easeOutBounce",onStep:function(e,i,a){$(this.el).find(".percent").text(Math.round(a))}})},{triggerOnce:!0,offset:"bottom-in-view"}),$.vegas("slideshow",{backgrounds:[{src:"http://resumex.themebucket.net/wp-content/uploads/2014/03/banner23.jpg",fade:1e3}]})("overlay",{src:"http://authenticgoods.co/wrapbootstrap/themes/articulate/img/overlays/16.png"}),$("#vegas-next").click(function(){$.vegas("next")}),$("#vegas-prev").click(function(){$.vegas("previous")}),$("#contact-form").validate({rules:{name:{minlength:2,required:!0},email:{required:!0,email:!0},message:{minlength:2,required:!0}},highlight:function(e){$(e).closest(".control-group").removeClass("success").addClass("error")},success:function(e){e.text("OK!").addClass("valid").closest(".control-group").removeClass("error").addClass("success")}})});
+        </script>
+
+        
+
+    </body>
+</html>
---
+++ wp-content/themes/resume/functions.php
@@ -0,0 +1,34 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.1.2' );
+
+// Add Viewport meta tag for mobile browsers
+add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
+function sample_viewport_meta_tag() {
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+}
+
+// Enable HTML5 markup
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+// Add support for custom background
+add_theme_support( 'custom-background' );
+
+// Add support for custom header
+add_theme_support( 'genesis-custom-header', array(
+	'width' => 1152,
+	'height' => 120
+) );
+
+// Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+
+
+/** Customize Genesis Footer */
+remove_action( 'genesis_footer', 'genesis_do_footer' );
